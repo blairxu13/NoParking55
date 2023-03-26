bba91a8f340be55dd0a0b0ce786a3c7fb35654be
jameslinejeudy <100732968+jameslinejeudy@users.noreply.github.com>
2023-03-26 10:28:51 -0400

    Update MergeSort.h

--- MergeSort.h
+++ MergeSort.h
@@ -1,14 +1,15 @@
-//
 // Created by UFSpring2023 on 3/20/2023.
-//
-
 #ifndef NOPARKING_MERGESORT_H
 #define NOPARKING_MERGESORT_H
 
 
-class MergeSort {
-
+class MergeSort{
+public:
+	void mergesort(vector<string> plates, int left, int right);
+private:
+	void merge(vector<string> plates, int left, int mid, int right);
 };
+
 
 
 #endif //NOPARKING_MERGESORT_H
