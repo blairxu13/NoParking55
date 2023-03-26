d6ea1e972b85c55df36cf1f0750eae0f71c56255
jameslinejeudy <100732968+jameslinejeudy@users.noreply.github.com>
2023-03-26 10:23:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector<License> totaldata;
+	vector<string> plates;
+	string search;
+
+	ifstream license;
+	license.open("parking_citations.csv");
+	string data;
+	getline(license, data);
+	while (getline(license, data, ',')) {
+		string platenumber = data;
+		(getline(license, data, ','));
+		string state = data;
+		(getline(license, data, ','));
+		string make = data;
+		(getline(license, data, ','));
+		string style = data;
+		(getline(license, data, ','));
+		string color = data;
+		(getline(license, data, ','));
+		string location = data;
+		(getline(license, data, ','));
+		string violation = data;
+		(getline(license, data, ','));
+		string fine = data;
+		(getline(license, data, ','));
+		string year = data;
+		(getline(license, data, ','));
+		string month = data;
+		(getline(license, data, ','));
+		string day = data;
+		(getline(license, data, ','));
+		string hour = data;
+		(getline(license, data, '\n'));
+		string minute = data;
+
+		License store(platenumber, state, location, make, style, color, violation, fine, year, month, day, hour, minute);
+
+
+		totaldata.push_back(store);
+		plates.push_back(platenumber);
+	}
+
+	//mergesorts
+	int size = plates.size();
+	Merge sort;
+	sort.mergesort(plates, 0, size - 1);
+
+	cout << "Enter License Plate: " << endl;
+	cin >> search;
+
+	return 0;
 }
